dea65da79224fd29749fd9cf333628b25dd36084
Odirlei Assis <odirlei_sabella@hotmail.com>
2024-05-27 22:29:11 -0300

    digitado funcionando + responsivel do glass

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function typeWritter(texto,idElemento,tempo){
-    var char = texto.split('').reverse();
-    var typer = setInterval(function () {
-        if (!char.length) return clearInterval(typer);
-        var next = char.pop();
-        document.getElementById(idElemento).innerHTML += next;
+var texto = 'A população ela precisa da Zona Franca de Manaus, porque na Zona franca de Manaus, não é uma zona de exportação, é uma zona para o Brasil. Portanto ela tem um objetivo, ela evita o desmatamento, que é altamente lucravito. Derrubar arvores da natureza é muito lucrativo!A população ela precisa da Zona Franca de Manaus, porque na Zona franca de Manaus, não é uma zona de exportação, é uma zona para o Brasil. Portanto ela tem um objetivo, ela evita o desmatamento, que é altamente lucravito. Derrubar arvores da natureza é muito lucrativo! Derrubar arvores da natureza é muito lucrativo!A população ela precisa da Zona Franca de Manaus, porque na Zona franca de Manaus, não é uma zona de exportação, é uma zona para o Brasil. Portanto ela tem um objetivo, ela evita o desmatamento, que é altamente lucravito. Derrubar arvores da natureza é muito lucrativo!';
+
+function escreverTexto(texto,idElemento,tempo){
+    var caracter = texto.split('').reverse();
+    var digitador = setInterval(function () {
+        if (!caracter.length) return clearInterval(digitador);
+        var proximo = caracter.pop();
+        document.getElementById(idElemento).innerHTML += proximo;
+        document.getElementById(idElemento).scrollTo(0, 1000);
     }, tempo);
+
 }
-typeWritter('Hoje está um lindo dia!','log',100);
+
+escreverTexto(texto,'texto',10);
