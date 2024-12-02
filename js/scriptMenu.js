175ba8985e875b605f69b8ecc81680e185aa3c0e
Odirlei <odirlei_sabella@hotmail.com>
2024-12-02 14:04:23 -0300

    codigo mais clean

--- js/scriptMenu.js
+++ js/scriptMenu.js
@@ -1,9 +1,5 @@
 function toggleNav(e) {
     e.preventDefault();
-    var nav = document.querySelector(".nav");
-    nav.classList.toggle("active");
-}
-
-function toggleSec(section) {
-    
+    var nav = document.querySelector(".container_menu");
+    nav.classList.toggle("ativo");
 }
